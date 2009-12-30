68890979a45c22b7b81c6dfd63cc008d8840ced7
askvictor <askvictor@localhost>
2009-12-30 05:18:51 +0000

    * added functionality to grab video metadata (frame size and duration) using hachoir-metadata, and store these as annotations * moved transcoding function out of subscribers.py into it's own file
    svn path=/plumi.content/trunk/; revision=107691

---
+++ plumi/content/metadataextractor.py
@@ -0,0 +1,58 @@
+#the extract function is ripped from p4a.video
+
+from zope.annotation.interfaces import IAnnotations
+
+from ZODB.PersistentMapping import PersistentMapping
+
+import logging
+logger = logging.getLogger('plumi.content')
+
+from hachoir_parser.guess import createParser
+from hachoir_metadata.metadata import extractMetadata
+from hachoir_core.error import HachoirError
+from hachoir_core.stream import InputStreamError
+
+def extract(filename):
+    """Extract the metadata from the media file"""
+
+    filename = unicode(filename)
+
+    try:
+        parser = createParser(filename)
+    except InputStreamError, err:
+        logger.error("stream error! %s\n" % unicode(err))
+        return None
+
+    if not parser:
+        logger.error("Unable to create parser.\n")
+        return None
+    try:
+        metadata = extractMetadata(parser)
+    except HachoirError, err:
+        logger.error("stream error! %s\n" % unicode(err))
+        return None
+
+    if metadata is None:
+        logger.error("unable to extract metadata.\n")
+        return None
+
+    return metadata
+
+def setup_metadata(obj):
+    annotations = IAnnotations(obj, None)
+    if not annotations.has_key('plumi.video_info'):
+        annotations['plumi.video_info'] = PersistentMapping()
+    video_info = annotations.get('plumi.video_info')
+
+    filename = obj.video_file.getBlob().committed()
+    metadata = extract(filename)
+    try:
+        video_info['width'] = metadata.get('width')
+        video_info['height'] = metadata.get('height')
+        video_info['aspect_ratio'] = video_info['width']/video_info['height']
+    except ValueError:
+        logger.error('Could not get video dimensions')
+    try:
+        video_info['duration'] = metadata.get('duration')
+    except ValueError:
+        logger.error('Could not get video duration')
---
+++ plumi/content/transcoding.py
@@ -0,0 +1,75 @@
+from zope.annotation.interfaces import IAnnotations
+from zope.component import getUtility
+
+import transaction
+from ZODB.PersistentMapping import PersistentMapping
+import xmlrpclib
+import urllib
+from urlparse import urlparse
+
+
+def setup_transcoding(obj):
+    annotations = IAnnotations(obj, None)
+    if not annotations.has_key('plumi.transcode.profiles'):
+        annotations['plumi.transcode.profiles'] = PersistentMapping()
+
+    pprop = getUtility(IPropertiesTool)
+    config = getattr(pprop, 'plumi_properties', None)
+    #TODO Check if we have transcoding support enabled
+    try:
+        transcodeServer=xmlrpclib.ServerProxy(config.transcodedaemon_address)
+        transcodeProfiles = transcodeServer.getAvailableProfiles()
+    except Exception, e:
+        print 'ERROR: Could not connect to transcode daemon %s: %s' % (config.transcodedaemon_address, e)
+        return
+    #Submit XML-RPC call to transcoder
+    transcodeOptions = dict()
+    
+    #TODO - get better way of discovering the path to the video file
+    path = obj.absolute_url_path()
+    if config.plonesite_address:
+        plonesite = urlparse(config.plonesite_address)
+    else:
+        plonesite = urlparse(obj.absolute_url())
+    url_format = "%s://%s:%s@%s%s/@@streaming_RPC"
+
+    cb_url = url_format % ( plonesite[0], #protocol
+                            config.plonesite_login,
+                            config.plonesite_password,
+                            plonesite[1], #netloc
+                            urllib.quote(path)) #path (for this video)
+                           
+    transcodeInput=dict(path = ( plonesite[0] + "://" + 
+                                 plonesite[1] + path + 
+                                 '/download/video_file/' + 
+                                 obj.video_file.getFilename()), 
+                        type=obj.video_file.getContentType())
+
+    trans = transaction.get()
+    for transcodeProfile in transcodeProfiles:
+        print "plumi running profile %s" % transcodeProfile
+
+        trans.addAfterCommitHook(launchConversion, 
+                                (transcodeServer, transcodeInput, 
+                                transcodeProfile, transcodeOptions, cb_url))
+    
+    #self.info ['state'] = "processing"
+
+    print "plumi: ConvertDaemon call pending"
+
+def launchConversion(status, server, input, profile, options, cb_url):
+    """
+    """
+    if not status:
+        return
+    try:
+        jobId = server.convert(input, profile, options, cb_url)
+        print "plumi: ConvertDaemon call "+jobId
+    #except xmlrpclib.Error, e:
+    except Exception, e:
+        print "plumi: ConvertDaemon call FAILED", e
+        server=xmlrpclib.ServerProxy(cb_url)
+        server.conv_done_xmlrpc(e)
+    #print "INPUT ", input_path
+    #print "OUTPUT", output_path
+    #return jobId
