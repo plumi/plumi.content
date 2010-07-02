6b5d2a228d924fd930fa94e85a81ec85a05e88f5
Dimitris Moraitis <dimo@unweb.me>
2010-07-02 21:13:02 +0000

    remove unnecessary utf-8 encode
    svn path=/plumi.content/trunk/; revision=120181

--- plumi/content/adapters.py
+++ plumi/content/adapters.py
@@ -184,7 +184,7 @@ class PlumiWorkflowAdapter(object):
             mTo = member.getProperty('email',None)
             if mTo is not None and mTo is not '':
                 mMsg = 'Hi %s \n' % member.getProperty('fullname', 'you')
-                mMsg += _('Your contribution has been accepted for publishing on the site\n').encode('utf-8','ignore')
+                mMsg += _('Your contribution has been accepted for publishing on the site\n')
                 mMsg += 'Title: %s\n\n' % obj_title
                 mMsg += '%s/view \n\n' % obj_url
                 urltool = getToolByName(self.context, 'portal_url')
