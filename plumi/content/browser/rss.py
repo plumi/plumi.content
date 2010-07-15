b0797304359f7b33b84c6d8a70a9e26e00b977b9
Markos Gogoulos <mgogoulos@gmail.com>
2010-07-15 18:52:51 +0000

    fixes RSS related bugs
    on Plumi
    
    
    
    svn path=/plumi.content/trunk/; revision=120836

--- plumi/content/browser/rss.py
+++
@@ -1,8 +0,0 @@
-from Products.Five.browser import BrowserView
-
-
-
-class RSSView(BrowserView):
-
-    def items(self):
-        return self.context.getFolderContents(contentFilter={"portal_type":"PlumiVideo", "review_state":['published', 'featured'], 'sort_on': 'effective', 'sort_order': 'reverse'})
