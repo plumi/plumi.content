f6c7a03b428c15242f3fa79d37bbf9d7e8f26165
Dimitris Moraitis <dimo@unweb.me>
2011-05-19 03:12:31 +0000

    Prepare plumi.content 4.2rc2.
    svn path=/plumi.content/trunk/; revision=239546

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.2rc1'
+version = '4.2rc2'
 
 long_description = (
     read('README.txt')
