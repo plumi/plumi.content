2d7ec6c7a20c4c1edd0dd0b466c8e07088e1b061
Rok Garbas <rok@garbas.si>
2011-09-12 06:55:15 +0200

    removing featured_videos_homepage from plumi.content (moving it into plumi.skin)

--- plumi/content/browser/interfaces.py
+++ plumi/content/browser/interfaces.py
@@ -36,32 +36,6 @@ class IVideoView( Interface ):
     bt_availability = Attribute("A dict giving info about the BT status of the"
                                 " file. It's a dict with two keys.")
 
-class ITopicsProvider( Interface ):
-    u"""The browser views implementing this interface have for mission
-    to retrieve a list of categories with all the linked infos from the
-    adapted object.
-    """
-    def get_categories_info(categories):
-        u"""Returns a list of dict representing a category.
-        The dict contains 3 strings : title, url and id.
-        @params : iterable of category ids
-        """
-
-    def get_country_info(countries):
-        u"""Returns a list of dict representing a country.
-        The dict contains 3 strings : title, url and id.
-        @params : iterable of country ids
-        """
-
-    __parent__ = Attribute(
-        """The view the provider appears in.
-
-        The view is the third discriminator of the content provider. It allows
-        that the content can be controlled for different views.
-
-        Having it stored as the parent is also very important for the security
-        context to be kept.
-        """)
 
 class IFeaturedVideosRetriever( IBrowserView, ITopicsProvider ):
     u"""This interface defines a featured videos retriever.
