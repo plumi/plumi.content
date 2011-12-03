09161c7280b788f257b2c860187fd33a0189fc27
Dimitris Moraitis <dimo@unweb.me>
2011-12-03 17:48:38 +0200

    add uninstall profile

--- plumi/content/setuphandlers.py
+++ plumi/content/setuphandlers.py
@@ -2,6 +2,7 @@ import logging
 from Products.CMFCore.utils import getToolByName
 from plumi.content.vocabs  import vocab_set as vocabs
 from Products.ATVocabularyManager.config import TOOL_NAME as ATVOCABULARYTOOL
+from Products.CMFPlone.utils import _createObjectByType
 
 def setupVocabs(portal, logger):
     #
@@ -15,9 +16,9 @@ def setupVocabs(portal, logger):
         # create vocabulary if it doesnt exist:
         vocabname = vkey
         if atvm.getVocabularyByName(vocabname):
-            atvm.manage_delObjects(vocabname)
+            atvm._delOb(vocabname)
         logger.debug("adding vocabulary %s" % vocabname)
-        atvm.invokeFactory('SimpleVocabulary', vocabname)
+        _createObjectByType('SimpleVocabulary', atvm, vocabname)
 
         vocab = atvm[vocabname]
 
@@ -27,7 +28,7 @@ def setupVocabs(portal, logger):
 
         for (ikey, value) in vocabs [vkey]:
             if not hasattr(vocab, ikey):
-                vocab.invokeFactory('SimpleVocabularyTerm', ikey)
+                _createObjectByType('SimpleVocabularyTerm', vocab, ikey)
                 logger.debug("adding vocabulary item %s %s" % (ikey,value))
                 vocab[ikey].setTitle(value)
 
@@ -47,3 +48,24 @@ def setupVarious(context):
     portal = context.getSite()
     logger = logging.getLogger('plumi.content')
     setupVocabs(portal, logger)
+    
+def uninstallVocabs(portal, logger):
+    #
+    #ATVocabManager setup
+    #
+    logger.info('Reverting ATVocabManager configuration ')
+    atvm = getToolByName(portal, ATVOCABULARYTOOL)
+
+    for vkey in vocabs.keys():
+        vocabname = vkey
+        if atvm.getVocabularyByName(vocabname):
+            atvm._delObject(vocabname)
+            logger.debug("removing vocabulary %s" % vocabname)
+            
+def uninstall(context):
+    if context.readDataFile('plumi.content_uninstall.txt') is None:
+        return
+    print "uninstalling plumi.content"
+    portal = context.getSite()    
+    logger = logging.getLogger('plumi.content')    
+    uninstallVocabs(portal, logger)    
