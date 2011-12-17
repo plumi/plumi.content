07f6bbf1c0534c211f98c6045ad08207da586cb3
Dimitris Moraitis <dimo@unweb.me>
2011-12-18 01:34:46 +0200

    get sources using http by default

--- plumi/content/tests/base.py
+++ plumi/content/tests/base.py
@@ -45,46 +45,18 @@ def setup_product():
     ztc.installProduct('PortalTransforms') 
     # to support tests for translated vocabularies
     ztc.installProduct('PloneLanguageTool')
-    #ztc.installProduct('LinguaPlone')    
     # Load the ZCML configuration for the example.tests package.
     # This can of course use <include /> to include other packages.
 
     import Products.ATVocabularyManager
     import plumi.content
-    #import plumi.skin
     fiveconfigure.debug_mode = True
     zcml.load_config('configure.zcml', Products.ATVocabularyManager)
-    zcml.load_config('configure.zcml', plumi.content)   
-    #zcml.load_config('configure.zcml', Products.LinguaPlone)    
-    #zcml.load_config('configure.zcml', plumi.skin)       
+    zcml.load_config('configure.zcml', plumi.content)        
     fiveconfigure.debug_mode = False
         
     ztc.installPackage('ATVocabularyManager')
     ztc.installPackage('plumi.content')
-    #ztc.installPackage('plumi.skin')    
-    
-    #import plumi.app
-    
-
-    #zcml.load_config('configure.zcml', plumi.app)
-    
-    #zcml.load_config('configure.zcml', Products.ATVocabularyManager)     
-    #fiveconfigure.debug_mode = False
-
-    # We need to tell the testing framework that these products
-    # should be available. This can't happen until after we have loaded
-    # the ZCML. Thus, we do it here. Note the use of installPackage()
-    # instead of installProduct().
-    # This is *only* necessary for packages outside the Products.*
-    # namespace which are also declared as Zope 2 products, using
-    # <five:registerPackage /> in ZCML.
-
-    # We may also need to load dependencies, e.g.:
-    #   ztc.installPackage('borg.localrole')
-    #ztc.installPackage('plumi.content')
-   
-    #ztc.installPackage('plumi.skin')    
-    #ztc.installPackage('plumi.app')
 
 # The order here is important: We first call the (deferred) function
 # which installs the products we need for this product. Then, we let
@@ -93,14 +65,7 @@ def setup_product():
 setup_product()
 ptc.installProduct('ATVocabularyManager')
 ptc.installProduct('plumi.content')
-ptc.setupPloneSite(products=('plumi.content',))
-
-#def installWithinPortal(portal):
-    #qi = getToolByName(portal, 'portal_quickinstaller')
-    #qi.installProduct('ATVocabularyManager')
-    #qi.installProduct('plumi.content')    
-    #qi.installProduct('LinguaPlone')    
-    #qi.installProduct('plumi.skin')        
+ptc.setupPloneSite(products=('plumi.content',))     
     
     
 class TestCase(ptc.PloneTestCase):
