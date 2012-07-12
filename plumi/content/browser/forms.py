834c0a09fe155471ae9c93e9c34e8f0ba53905b1
Dimitris Moraitis <dimo@unweb.me>
2012-07-12 20:21:37 +0300

    enable license widget

--- plumi/content/browser/forms.py
+++ plumi/content/browser/forms.py
@@ -12,12 +12,13 @@ from zope.schema import ValidationError
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.schema.interfaces import IContextSourceBinder
 from zope.interface import alsoProvides
-from zope.component import getUtility
+from zope.component import getUtility, adapter
 from zope.event import notify
 
+from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+
 from z3c.form import button
-from z3c.form.browser.image import ImageWidget
-from collective.contentlicensing.browser.widgets import LicenseWidget
+from z3c.form import widget
 
 from five import grok
 
@@ -28,11 +29,15 @@ from plone.z3cform.interfaces import IWrappedForm
 
 from Products.Archetypes.config import RENAME_AFTER_CREATION_ATTEMPTS
 from Products.Archetypes.event import ObjectInitializedEvent
+from Products.Archetypes.event import ObjectModifiedEvent
 from Products.CMFDefault.utils import checkEmailAddress
 from Products.CMFDefault.exceptions import EmailAddressInvalid
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.interfaces import ISiteRoot, IFolderish
 from Products.statusmessages.interfaces import IStatusMessage
+
+from collective.contentlicensing.utilities.interfaces import IContentLicensingUtility
+from collective.contentlicensing.DublinCoreExtensions.interfaces import ILicense
 
 from plumi.content import plumiMessageFactory as _
 from plumi.content.interfaces import IPlumiVideoFolder
@@ -41,8 +46,10 @@ from plumi.content.interfaces import IPlumiVideoFolder
 class InvalidEmailAddress(ValidationError):
     "Invalid email address"
 
+
 class InvalidImage(ValidationError):
     "Please upload a valid image file"
+
 
 def validateaddress(value):
     try:
@@ -50,6 +57,7 @@ def validateaddress(value):
     except EmailAddressInvalid:
         raise InvalidEmailAddress(value)
     return True
+
 
 def validateimage(value):
     try:
@@ -60,13 +68,14 @@ def validateimage(value):
 
 
 @grok.provider(IContextSourceBinder)
-def get_video_languages(context):  
+def get_video_languages(context):
     return get_vocabulary_items(context, 'video_languages')
 
 
 @grok.provider(IContextSourceBinder)
 def get_video_countries(context):
     return get_vocabulary_items(context, 'video_countries')
+
 
 @grok.provider(IContextSourceBinder)
 def get_video_genres(context):
@@ -84,159 +93,134 @@ def get_vocabulary_items(context, vocabulary):
     voc = pv.getVocabularyByName(vocabulary)
     countriesDict = []
     voc_terms = voc.getDisplayList(context).items()
-    countriesDict = [ SimpleTerm(value=term[0], token=term[0], title=term[1]) for term in voc_terms]
+    countriesDict = [SimpleTerm(value=term[0], token=term[0], title=term[1])
+                     for term in voc_terms]
     return SimpleVocabulary(countriesDict)
 
 
 class IPlumiVideo(form.Schema):
-    
-    form.fieldset('default',
-            label=u"Basic info",
-            fields=['Title','Description', 'DateProduced','Language', 'FullDescription', 'Thumbnail', 'License']
-        )
-    
+    """ Publish video form schema """
+
     form.fieldset('categorize',
-            label=u"Categorise",
-            fields=['Genre', 'Country', 'Location', 'Topics', 'Tags', 'Director',
-                'Producer', 'Email', 'Organisation', 'ProductionCompany', 'Website']
-        )
+                  label=u"Categorise",
+                  fields=['Genre', 'Country', 'Location', 'Topics', 'Tags',
+                          'Director', 'Producer', 'Email', 'Organisation',
+                          'ProductionCompany', 'Website']
+                  )
 
-    Title = schema.TextLine(
-            title=_(u"Title"),
-            min_length=1,
-            max_length=160,
-            required=True,
-        )
+    Title = schema.TextLine(title=_(u"Title"),
+                            max_length=160,
+                            required=True,
+                            )
 
-    Description = schema.Text(
-        title=_(u"Short summary"), 
-        required=True,
-        min_length=10, 
-        max_length=160,
-        description=_(u"Describe your video in 160 characters."),
-    )
-    
-    DateProduced = schema.Date(
-        title=_(u"Date Produced"), 
-        required=True,
-        description=_(u"The date the video content was released."),
-    )
+    Description = schema.Text(title=_(u"Short summary"),
+                              required=True,
+                              max_length=160,
+                              description=_(u"Describe your video in 160 characters."),
+                              )
 
-    Language = schema.Choice(
-        title=_(u"Video Language"),
-        required=True,
-        default= 'en',
-        source=get_video_languages,
-    )
+    DateProduced = schema.Date(title=_(u"Date Produced"),
+                               required=True,
+                               description=_(u"The date the video content was released."),
+                               )
+
+    Language = schema.Choice(title=_(u"Video Language"),
+                             required=True,
+                             default='en',
+                             source=get_video_languages,
+                             )
 
     form.widget(FullDescription=WysiwygFieldWidget)
-    FullDescription = schema.Text(
-        title=_(u"Full Description"), 
-        required=False,
-    )
+    FullDescription = schema.Text(title=_(u"Full Description"),
+                                  required=False,
+                                  )
 
     #FIX: find a more native validation -eg provided by zope.schema
     Thumbnail = schema.Bytes(title=u'Add thumbnail',
-                         constraint=validateimage,
-                         description=u"We will automatically generate an image, but you may prefer to upload your own",
-                         required=False)
+                             constraint=validateimage,
+                             description=u"We will automatically generate an image, but you may prefer to upload your own",
+                             required=False,
+                             )
 
-    #FIX: proper widget
-    #form.widget(License=LicenseWidget)
-    License = schema.TextLine(
-            title=_(u"License"),
-            required=False,
-        )
+    License = schema.TextLine(title=_(u"License"),
+                              required=False,
+                              )
 
-    Genre = schema.Choice(
-            title=_(u"Genre"),
-            required=False,
-            source=get_video_genres,
-            default='documentary',
-        )
+    Genre = schema.Choice(title=_(u"Genre"),
+                          required=False,
+                          source=get_video_genres,
+                          default='documentary',
+                          )
 
-    Country = schema.Choice(
-            title=_(u"Country"),
-            required=False,
-            default= 'XX',
-            source=get_video_countries
-        )
+    Country = schema.Choice(title=_(u"Country"),
+                            required=False,
+                            default='XX',
+                            source=get_video_countries
+                            )
 
-    Location = schema.TextLine(
-            title=_(u"Location"),
-            description=_(u"e.g. City or Region."),
-            required=False,
-        )
+    Location = schema.TextLine(title=_(u"Location"),
+                               description=_(u"e.g. City or Region."),
+                               required=False,
+                               )
 
-    Topics = schema.List(
-            title=_(u"Topics"),
-            required=False,
-            value_type=schema.Choice(source=get_video_categories),
-            default=[],
-        )
+    Topics = schema.List(title=_(u"Topics"),
+                         required=False,
+                         value_type=schema.Choice(source=get_video_categories),
+                         default=[],
+                         )
 
-    Tags = schema.TextLine(
-            title=_(u"Tags"),
-            description=_(u"Seperate with comma. Eg tag1, tag2, tag4"),
-            required=False,
-        )
+    Tags = schema.TextLine(title=_(u"Tags"),
+                           description=_(u"Seperate with comma. Eg tag1, tag2, tag4"),
+                           required=False,
+                           )
 
-    Director = schema.TextLine(
-            title=_(u"Director"),
-            required=False,
-        )
+    Director = schema.TextLine(title=_(u"Director"),
+                               required=False,
+                               )
 
-    Producer = schema.TextLine(
-            title=_(u"Producer"),
-            required=False,
-        )
+    Producer = schema.TextLine(title=_(u"Producer"),
+                               required=False,
+                               )
 
-    Email = schema.TextLine(
-            title=_(u"Email Address"),
-            required=False,
-            constraint=validateaddress,
-        )
+    Email = schema.TextLine(title=_(u"Email Address"),
+                            required=False,
+                            constraint=validateaddress,
+                            )
 
-    Organisation = schema.TextLine(
-            title=_(u"Project Name"),
-            required=False,
-        )
+    Organisation = schema.TextLine(title=_(u"Project Name"),
+                                   required=False,
+                                   )
 
+    ProductionCompany = schema.TextLine(title=_(u"Production Company"),
+                                        required=False,
+                                        )
 
-    ProductionCompany = schema.TextLine(
-            title=_(u"Production Company"),
-            required=False,
-        )
-
-    Website = schema.URI(
-            title=_(u"Website URL"),
-            required=False,
-        )
+    Website = schema.URI(title=_(u"Website URL"),
+                         required=False,
+                         )
 
 
 class VideoAddForm(form.SchemaForm):
     grok.name('publish_video')
     grok.require('cmf.AddPortalContent')
     grok.context(IFolderish)
-        
+
     schema = IPlumiVideo
     ignoreContext = True
- 
+
     label = _(u"Publish your video")
     #description = _(u"...")
-
 
     def uploaded_file(self):
         session_path = tempfile.gettempdir() + '/' + 'plumitmp/' + self.request['SESSION'].id
         try:
             filename = os.listdir(session_path)[0]
-            return {'filename' : filename, 
-                    'filesize' : os.stat(session_path + '/' + filename).st_size,
-                    'path' : session_path + '/' + filename}
+            return {'filename': filename,
+                    'filesize': os.stat(session_path + '/' + filename).st_size,
+                    'path': session_path + '/' + filename}
         except:
             return None
 
-    
     def _findUniqueId(self, id):
         """Find a unique id in the current folder, based on the given id, by
         appending -n, where n is a number between 1 and the constant
@@ -244,7 +228,7 @@ class VideoAddForm(form.SchemaForm):
         found, return None.
         """
         ids = self.context.objectIds()
-        
+
         if id not in ids:
             return id
 
@@ -256,30 +240,30 @@ class VideoAddForm(form.SchemaForm):
             idx += 1
 
         return None
-    
-        
+
     def update(self):
         # disable Plone's editable border
         self.request.set('disable_border', True)
         # call the base class version - this is very important!
         super(VideoAddForm, self).update()
-    
+
     @button.buttonAndHandler(_(u'Save changes'))
     def handleApply(self, action):
         data, errors = self.extractData()
+
         if errors:
             self.status = self.formErrorsMessage
             return
-        
+
         if not self.uploaded_file():
             self.status = _(u"No file was uploaded")
             return
-        
+
         # Calculate unique id from title
         uid = str(DateTime().millis())
 
         if data['Tags']:
-            subject = data['Tags'].replace(' ','').split(',')
+            subject = data['Tags'].replace(' ', '').split(',')
         else:
             subject = ''
 
@@ -302,38 +286,43 @@ class VideoAddForm(form.SchemaForm):
                                    ProjectName=data['Organisation'] or '',
                                    WebsiteURL=data['Website'] or '',
                                    )
-        
+
         # get the newly created object
         obj = self.context[uid]
-        
+
         # set the video file field and remove temp uploaded file
         path = self.uploaded_file()['path']
         f = open(path)
         obj.setFile(f)
         f.close()
         shutil.rmtree('/'.join(path.split('/')[:-1]), ignore_errors=True)
-        
+
         # Rename with friendly id
         normalizer = getUtility(IIDNormalizer)
         new_id = self._findUniqueId(normalizer.normalize(data['Title']))
         transaction.savepoint(1)
         obj.setId(new_id)
-        
+
         # Reindex and send init event
         obj.reindexObject()
         notify(ObjectInitializedEvent(obj))
-        
+        notify(ObjectModifiedEvent(obj))
+
         # Redirect back to the front page with a status message
-        IStatusMessage(self.request).addStatusMessage(
-                _(u"Thank you very much for your contribution! We will review the video and notify you once it is ready."), 
-                "info"
-            )
+        IStatusMessage(self.request).addStatusMessage(_(u"Thank you very much for your contribution! We will review the video and notify you once it is ready."),
+                                                      "info"
+                                                      )
         contextURL = obj.absolute_url()
         self.request.response.redirect(contextURL)
-        
+
     @button.buttonAndHandler(_(u"Cancel"))
     def handleCancel(self, action):
         """User cancelled. Redirect back to the front page.
         """
         contextURL = self.context.absolute_url()
         self.request.response.redirect(contextURL)
+
+    def updateWidgets(self):
+        super(VideoAddForm, self).updateWidgets()
+        self.default_fieldset_label = _('Basic info')
+        self.widgets["License"].template = ViewPageTemplateFile("forms_templates/ccwidget.pt")
