78c64aed4bc69f9d13ccec75f4f33030c3400817
Dimitris Moraitis <dimo@unweb.me>
2012-07-26 20:33:05 +0300

    move featured templates from plumi.skin

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.4.1'
+version = '4.5dev'
 
 long_description = (
     read('README.txt')
