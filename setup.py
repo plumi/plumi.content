11bcb31ffdd897933895ac6a4440d9aebdccd58f
Sam Stainsby <sam@stainsby.id.au>
2014-10-13 15:23:46 +1000

    added correct ID for player element to work with plumi.skin video template

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.5.2-externalvideo-pre2'
+version = '4.5.2-externalvideo-pre3'
 
 long_description = (
     read('README.txt')
