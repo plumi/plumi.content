31f69ae122344311aaf7c4fb64c9b1a65415fe73
Sam Stainsby <sam@stainsby.id.au>
2014-12-16 14:38:21 +1000

    increment externalvideo version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '4.5.2-externalvideo-pre4'
+version = '4.5.2-externalvideo-pre5'
 
 long_description = (
     read('README.txt')
