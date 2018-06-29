a5b9e09a490d9b64f32da9fc2de9fb1469f18cae
Markos Gogoulos <mgogoulos@mist.io>
2018-06-29 12:09:33 -0700

    changes over time

--- plumi/content/browser/forms.py
+++ plumi/content/browser/forms.py
@@ -14,6 +14,8 @@ from zope.component import getUtility, adapter
 from zope.event import notify
 
 from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from zope.intid.interfaces import IIntIds
+from z3c.relationfield.relation import RelationValue
 
 from z3c.form import button
 from z3c.form import widget
@@ -33,6 +35,7 @@ from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.interfaces import ISiteRoot, IFolderish
 from Products.statusmessages.interfaces import IStatusMessage
 from plone.registry.interfaces import IRegistry
+from plone.app.textfield.value import RichTextValue
 
 from collective.contentlicensing.utilities.interfaces import IContentLicensingUtility
 from collective.contentlicensing.DublinCoreExtensions.interfaces import ILicense
@@ -345,6 +348,245 @@ class VideoAddForm(form.SchemaForm):
 
     def updateWidgets(self):
         super(VideoAddForm, self).updateWidgets()
+        self.default_fieldset_label = _('Basic info')
+        self.widgets["License"].template = ViewPageTemplateFile("forms_templates/ccwidget.pt")
+
+    def create_object(self, context, data, uid, subject):
+        context.invokeFactory('PlumiVideo', id=uid,
+                                   description=data['Description'],
+                                   DateProduced=data['DateProduced'],
+                                   VideoLanguage=data['Language'],
+                                   FullDescription=data['FullDescription'],
+                                   thumbnailImage=data['Thumbnail'],
+                                   Genre=data['Genre'],
+                                   Countries=data['Country'],
+                                   location=data['Location'],
+                                   Categories=data['Topics'],
+                                   subject=subject,
+                                   Director=data['Director'] or '',
+                                   Producer=data['Producer'] or '',
+                                   ProducerEmail=data['Email'] or '',
+                                   ProductionCompanyName=data['ProductionCompany'] or '',
+                                   ProjectName=data['Organisation'] or '',
+                                   WebsiteURL=data['Website'] or '',
+                                   )
+
+def is_valid_url(url):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    if not regex.search(url):
+        return False
+    return True
+
+from HTMLParser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+class ScalarVideoAddForm(form.SchemaForm):
+    grok.name('publish_video_scalar')
+    grok.require('cmf.AddPortalContent')
+    grok.context(IFolderish)
+
+    schema = IPlumiVideo
+    ignoreContext = True
+
+    label = _(u"Publish your video")
+    #description = _(u"...")
+
+    def uploaded_file(self):
+        pm = getToolByName(self.context,'portal_membership')
+        userId = pm.getAuthenticatedMember().id
+        session_path = tempfile.gettempdir() + '/' + 'plumitmp/' + userId
+        try:
+            filename = os.listdir(session_path)[0]
+            return {'filename': filename,
+                    'filesize': os.stat(session_path + '/' + filename).st_size,
+                    'path': session_path + '/' + filename}
+        except:
+            return None
+
+    def _findUniqueId(self, id):
+        """Find a unique id in the current folder, based on the given id, by
+        appending -n, where n is a number between 1 and the constant
+        RENAME_AFTER_CREATION_ATTEMPTS. If no id can be
+        found, return None.
+        """
+        ids = self.context.objectIds()
+
+        if id not in ids:
+            return id
+
+        idx = 1
+        while idx <= RENAME_AFTER_CREATION_ATTEMPTS:
+            new_id = "%s-%d" % (id, idx)
+            if not new_id in ids:
+                return new_id
+            idx += 1
+
+        return None
+
+    def update(self):
+        # disable Plone's editable border
+        self.request.set('disable_border', True)
+        # call the base class version - this is very important!
+        super(ScalarVideoAddForm, self).update()
+
+    @button.buttonAndHandler(_(u'Save'), name='apply')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            if len(errors) == 1:
+                if errors[0].field.getName() == "Email":
+                    self.status = _(u'Invalid E-mail address')
+                elif errors[0].field.getName() == "Website":
+                    self.status = _(u'Invalid Website URI')
+            else:
+                self.status = self.formErrorsMessage
+            return
+
+        if not self.uploaded_file():
+            self.status = _(u"No file was uploaded")
+            return
+
+        # Calculate unique id from title
+        uid = str(DateTime().millis())
+
+        if data['Tags']:
+            subject = data['Tags'].replace(' ', '').split(',')
+        else:
+            subject = ''
+        
+        try:
+            if data['Website']:
+                if not data['Website'].startswith('http://'):
+                    data['Website'] = 'http://' + data['Website']
+        except:
+            pass
+        self.create_object(self.context, data, uid, subject)
+
+        # get the newly created object
+        obj = self.context[uid]
+
+        # set the video file field and remove temp uploaded file
+        path = self.uploaded_file()['path']
+        f = open(path)
+        obj.setFile(f)
+        f.close()
+        shutil.rmtree('/'.join(path.split('/')[:-1]), ignore_errors=True)
+
+        # Rename with friendly id
+        normalizer = getUtility(IIDNormalizer)
+        new_id = self._findUniqueId(normalizer.normalize(data['Title']))
+        video_id = new_id
+        transaction.savepoint(1)
+        obj.setId(new_id)
+        obj.setTitle(data['Title'])
+
+        # Reindex and send init event
+        obj.reindexObject()
+        notify(ObjectInitializedEvent(obj))
+
+
+        if data.get('CommentaryDescription') and data.get('CommentaryTitle'):         
+            pm = getToolByName(obj, 'portal_membership')
+            home = pm.getHomeFolder()
+            title = unicode(strip_tags(data.get('CommentaryTitle')))[:200]
+            body = data.get('CommentaryDescription')
+
+            normalizer = getUtility(IIDNormalizer)
+            try:
+                target_folder = home.commentaries
+                new_id = target_folder.invokeFactory(id=str(DateTime().millis()), type_name='Commentary', title=title)
+                commentary_obj = target_folder[new_id]
+                commentary_obj.textCommentary = RichTextValue(unicode(strip_tags(body))[:20000], 'text/plain', 'text/plain', 'utf-8')
+                RelatedItems = obj.getRelatedItems()
+                RelatedItems.append(commentary_obj)
+                obj.setRelatedItems(RelatedItems)
+                #used to save the related items
+                #also add the video to the commentary's related items, to be used on the subscribers for when deleting a commentary
+                obj.reindexObject()
+                wft = getToolByName(obj, 'portal_workflow')
+                for state in ['submit','show','process','publish']:
+                    try:
+                        wft.doActionFor(obj, state)
+                    except:
+                        pass
+                try:
+                    # SOS: why this not working...
+                    intid = getUtility(IIntIds).getId(obj)
+                    relatedVids = [RelationValue(intid)]
+                    commentary_obj.relatedItems = relatedVids
+                except KeyError:
+                    try:
+                        to_id = getUtility(IIntIds).register(obj)
+                        relatedVids = [RelationValue(to_id)]
+                        commentary_obj.relatedItems = relatedVids
+                    except:
+                        pass
+            except Exception as e:
+                pass
+
+            wft = getToolByName(obj, 'portal_workflow')
+            state = wft.getInfoFor(obj, 'review_state')
+            try:
+                if state == "private":
+                    try:
+                        wft.doActionFor(obj, 'show')
+                    except:
+                        pass
+                    try:
+                        wft.doActionFor(obj, 'submit')
+                    except:
+                        pass
+                    try:
+                        wft.doActionFor(obj, 'publish')
+                    except:
+                        pass
+            except: 
+                pass
+
+        # Redirect back to the front page with a status message
+        registry = getUtility(IRegistry)
+        message = registry['plumi.content.browser.interfaces.IPlumiSettings.AfterVideoText']
+        IStatusMessage(self.request).addStatusMessage(message,"info")
+        contextURL = obj.absolute_url()
+
+        try:
+            ref = self.request.HTTP_REFERER
+            if 'prev=' in ref:
+                redirect_url = ref.split('prev=')[1]
+                url = "%s?%s" % (redirect_url, contextURL)
+                self.request.response.redirect(url)
+        except:
+            pass
+        self.request.response.redirect(contextURL)
+
+    @button.buttonAndHandler(_(u"Cancel"))
+    def handleCancel(self, action):
+        """User cancelled. Redirect back to the front page.
+        """
+        contextURL = self.context.absolute_url()
+        self.request.response.redirect(contextURL)
+
+    def updateWidgets(self):
+        super(ScalarVideoAddForm, self).updateWidgets()
         self.default_fieldset_label = _('Basic info')
         self.widgets["License"].template = ViewPageTemplateFile("forms_templates/ccwidget.pt")
 
